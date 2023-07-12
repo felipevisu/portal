5cec3960bfa23a2829ad9c88772002c49f68dc3a
Felipe Faria <felipevisu@gmail.com>
2023-07-12 14:48:16 -0300

    refactor: improve auth backend and middleware

--- portal/core/middleware.py
+++ portal/core/middleware.py
@@ -1,8 +1,13 @@
+import logging
 from datetime import datetime
 
 from django.conf import settings
 
 from .jwt import JWT_REFRESH_TOKEN_COOKIE_NAME, jwt_decode_with_exception_handler
+
+Requestor = "User"
+
+logger = logging.getLogger(__name__)
 
 
 def jwt_refresh_token_middleware(get_response):
@@ -12,20 +17,20 @@ def jwt_refresh_token_middleware(get_response):
         jwt_refresh_token = getattr(request, "refresh_token", None)
         if jwt_refresh_token:
             expires = None
+            secure = not settings.DEBUG
             if settings.JWT_EXPIRE:
                 refresh_token_payload = jwt_decode_with_exception_handler(
                     jwt_refresh_token
                 )
                 if refresh_token_payload and refresh_token_payload.get("exp"):
-                    expires = datetime.utcfromtimestamp(
-                        refresh_token_payload.get("exp")
-                    )
+                    expires = datetime.utcfromtimestamp(refresh_token_payload["exp"])
             response.set_cookie(
                 JWT_REFRESH_TOKEN_COOKIE_NAME,
                 jwt_refresh_token,
                 expires=expires,
                 httponly=True,  # protects token from leaking
-                secure=not settings.DEBUG,
+                secure=secure,
+                samesite="None" if secure else "Lax",
             )
         return response
 
--- portal/graphql/context.py
+++ portal/graphql/context.py
@@ -6,10 +6,10 @@ from django.http import HttpRequest
 from django.utils import timezone
 from django.utils.functional import SimpleLazyObject
 
-from portal.core.jwt import get_token_from_request, jwt_decode_with_exception_handler
-
 from ..account.models import User
-from .core.context import PortalContext
+from ..core.auth import get_token_from_request
+from ..core.jwt import jwt_decode_with_exception_handler
+from .core import PortalContext
 
 
 def get_context_value(request: HttpRequest) -> PortalContext:
