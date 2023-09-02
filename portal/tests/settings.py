bb9ef7500d52cf45be252cc22c612dcb0c7a26fe
Felipe Faria <felipevisu@gmail.com>
2023-09-02 15:48:54 -0300

    test: adjust test to tenant app

--- portal/tests/settings.py
+++ portal/tests/settings.py
@@ -1,4 +1,4 @@
-from ..settings import *  # noqa
+from ..settings import *
 
 SECRET_KEY = "NOTREALLY"
 
@@ -6,10 +6,15 @@ ALLOWED_CLIENT_HOSTS = ["www.example.com"]
 
 EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
 
-INSTALLED_APPS.append("portal.tests")  # noqa
+INSTALLED_APPS.append("portal.tests")
+INSTALLED_APPS.remove("django_tenants")
+
+MIDDLEWARE.remove("django_tenants.middleware.TenantSubfolderMiddleware"),
+
+DATABASE_ROUTERS = []
 
 AUTH_PASSWORD_VALIDATORS = []
 
-DATABASE_CONNECTION_REPLICA_NAME = DATABASE_CONNECTION_DEFAULT_NAME  # noqa
+DATABASE_CONNECTION_REPLICA_NAME = DATABASE_CONNECTION_DEFAULT_NAME
 
 JWT_EXPIRE = True
