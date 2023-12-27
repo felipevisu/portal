c023b0232bba508858384f031b4025c90ce03123
Felipe Faria <felipevisu@gmail.com>
2023-12-27 12:33:21 -0300

    feat: create entry types with attributes

--- portal/graphql/entry/mutations/entry_types.py
+++ portal/graphql/entry/mutations/entry_types.py
@@ -1,6 +1,9 @@
 import graphene
 from django.core.exceptions import ValidationError
 
+from portal.attribute import AttributeType
+from portal.graphql.core.types.common import NonNullList
+
 from ....core.permissions import EntryPermissions
 from ....entry import models
 from ...core.mutations import ModelDeleteMutation, ModelMutation
@@ -11,6 +14,11 @@ from ..types import EntryType
 class EntryTypeInput(graphene.InputObjectType):
     name = graphene.String()
     slug = graphene.String()
+    entry_attributes = NonNullList(
+        graphene.ID,
+        description="List of attributes shared among all entries.",
+        name="entryAttributes",
+    )
 
 
 class EntryTypeCreate(ModelMutation):
@@ -33,10 +41,38 @@ class EntryTypeCreate(ModelMutation):
             )
         except ValidationError as error:
             raise ValidationError({"slug": error})
+        cls.validate_attributes(cleaned_input)
         return cleaned_input
 
+    @classmethod
+    def validate_attributes(cls, cleaned_data):
+        errors = {}
+        for field in ["entry_attributes"]:
+            attributes = cleaned_data.get(field)
+            if not attributes:
+                continue
+            not_valid_attributes = [
+                graphene.Node.to_global_id("Attribute", attr.pk)
+                for attr in attributes
+                if attr.type != AttributeType.ENTRY_TYPE
+            ]
+            if not_valid_attributes:
+                errors[field] = ValidationError(
+                    "Only Entry type attributes are allowed.",
+                    params={"attributes": not_valid_attributes},
+                )
+        if errors:
+            raise ValidationError(errors)
 
-class EntryTypeUpdate(ModelMutation):
+    @classmethod
+    def _save_m2m(cls, info, instance, cleaned_data):
+        super()._save_m2m(info, instance, cleaned_data)
+        entry_attributes = cleaned_data.get("entry_attributes")
+        if entry_attributes is not None:
+            instance.entry_attributes.set(entry_attributes)
+
+
+class EntryTypeUpdate(EntryTypeCreate):
     entry_type = graphene.Field(EntryType)
 
     class Arguments:
